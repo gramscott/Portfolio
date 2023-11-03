7b8d43f469ba442b8773f4fdc3c51bbd4e5b27b7
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-03 16:10:56 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 import HomeContainer from './containers/HomeContainer';
 
 function App() {
   return (
     <div className="App">
+    <NavBar/>
     <HomeContainer />
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+
+  return (
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/contact">Contact</a>
+    </nav>
+  )
+
+}
+
+export default NavBar
