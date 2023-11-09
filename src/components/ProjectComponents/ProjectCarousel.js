3727c8d27ee8c67ad9f99f9d42397992da8ea978
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-09 10:24:38 +0000

    carousel completed

--- src/components/ProjectComponents/ProjectCarousel.js
+++ src/components/ProjectComponents/ProjectCarousel.js
@@ -1,78 +1,86 @@
-import React, {useRef} from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import './ProjectCarousel.css'
-import smecoPic from '../../images/carousel-smeco-pic.png'
-import empowerMePic from '../../images/carousel-empowerme-pic.png'
+import React, {useRef} from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './ProjectCarousel.css'; 
+import smecoPic from '../../images/carousel-smeco-pic.png';
+import empowerMePic from '../../images/carousel-empowerme-pic.png';
+import bonniePic from '../../images/carousel-bonnie-pic.jpg';
+
 
 const ProjectCarousel = () => {
 
-  const sliderRef = useRef(null);
+  const sliderRef = useRef(null); 
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  };
+    const settings = {
+      dots: false, 
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
 
-  const next = () => {
-    sliderRef.current.slickNext();
-  };
-
-  const previous = () => {
-    sliderRef.current.slickPrev();
-  };
-
+    const goToNextSlide = () => {
+      sliderRef.current.slickNext(); 
+    };
+  
+    const goToPrevSlide = () => {
+      sliderRef.current.slickPrev();
+    };
 
   return (
     <div className='project-carousel'>
-  <Slider ref={sliderRef} {...settings}>
-    <div className='project-slide'>
-
-      <img src={smecoPic} alt='pic-1' className='img-fluid pics' />
-      <b>Full-Stack JavaScript Project: smeco</b>
-      <p>
-        React, JavaScript, VS Code, API, Express.js, Cypress, JSX, Unit-Testing. Group [5 days]
-      </p>
-      <ul>
-        <li>
-        Collaborated to raise awareness about carbon footprints and provide users with tips to reduce their environmental impact.
-        </li>
-        <li>
-        Implemented features such as a multiple-choice quiz and a community page for users to collect tips and engage with like-minded individuals.
-        </li>
-        <li>Contributed to database creation and front-end development.</li>
-        <a href='https://github.com/gramscott/smeco'>
-          <i className='fab fa-github'>GitHub Repository</i>
-        </a>
-      </ul>
-    </div>
-
-    <div className='project-slide'>
-      <img src={empowerMePic} alt='pic-2' className='img-fluid pics' />
-      <b>Full-Stack Java Project: Empower Me</b>
-      <p>
-        Java, Spring Boot, IntelliJ, API, JUnit, JavaScript, Vs Code, React, MongoDB HTML, CSS. Group [5 days]
-      </p>
-      <ul>
-          <li>Collaborated to create a personalized self-help affirmation app where users could customize their positive affirmations and establish a safe space.</li>
-          <li>Created the Java backend and successfully connected the React frontend through RESTful routes.</li>
-          <li>Seeded and handled enormous data loader for presentation.</li>
-          <a href="https://github.com/gramscott/eMpowerMe">
-          <i className="fa fa-github"></i> GitHub Repository
+       <Slider {...settings} ref={sliderRef}>
+       <div className="project-slide">
+        <img src={smecoPic} alt="pic-1" className="img-fluid pic-1" />
+        <b>Full-Stack JavaScript Project: Smeco</b>
+        <p>React, JavaScript, VS Code, API, Express.js, Cypress, JSX, Unit-Testing. Group [5 days]</p>
+        <ul>
+          <li>Collaborated to raise awareness about carbon footprints and provide users with tips to reduce their environmental impact.</li>
+          <li>Implemented features such as a multiple-choice quiz and a community page for users to collect tips and engage with like-minded individuals.</li>
+          <li>Contributed to database creation and front-end development.</li>
+          <a href="https://github.com/gramscott/smeco">
+            <i className="fa fa-github"></i> GitHub Repository
           </a>
         </ul>
       </div>
 
+      <div className="project-slide">
+        <img src={empowerMePic} alt="pic-2" className="img-fluid pic-2" />
+        <b>Full-Stack Java Project: eMpowerMe</b>
+        <p>Java, IntelliJ IDEA, VS Code, React, Git, GitHub, Spring, CSS, JavaScript, Express, RESTful API’s, JSX, MongoDB, MYSQL. Pair [13 days]</p>
+        <ul>
+          <li>Collaborated to create a personalized self-help affirmation app where users could customize their positive affirmations and establish a safe space.</li>
+          <li>Created the Java backend and successfully connected the React frontend through RESTful routes.</li>
+          <li>Seeded and handled enormous data loader for presentation.</li>
+          <a href="https://github.com/gramscott/eMpowerMe">
+            <i className="fa fa-github"></i> GitHub Repository
+          </a>
+        </ul>
+      </div>
 
-  </Slider>
-</div>
+      <div className="project-slide">
+        <img src={bonniePic} alt="pic-3" className="img-fluid pic-3" />
+        <b>Full-Stack Python Project: My Bonnie Scotland</b>
+        <p>Python, HTML, CSS, SQL, Unit Testing, VS Code. Solo [5 days]</p>
+        <ul>
+          <li>Created a hypothetical CRUD app</li>
+          <li>“Filmmakers” can pencil in the filming sets they have worked on and the ones they would like to work on.</li>
+          <a href="https://github.com/gramscott/Solo-project-">
+            <i className="fa fa-github"></i> GitHub Repository
+          </a>
+        </ul>
+      </div>
+    </Slider>
 
-    
-  )
-}
+    <div className="button-navigation">
+    <button className="prev-button" onClick={goToPrevSlide}>Previous</button>
+    <button className="next-button" onClick={goToNextSlide} >Next</button>
+  </div>
+    </div>
+   
+  );
+};
 
-export default ProjectCarousel
+export default ProjectCarousel;
--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProjectCarousel from '../components/ProjectComponents/ProjectCarousel'
 
+
 const ProjectContainer = () => {
   return (
     <ProjectCarousel/>
