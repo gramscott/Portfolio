f938f1340969b936d2f899df5d40a641b08f5380
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-09 19:20:32 +0000

    form for the user interface is set up just tweaking the functionality

--- src/components/ContactComponents/ContactForm.js
+++ src/components/ContactComponents/ContactForm.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ContactForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleChange = (event) => {
+        setFormState({
+            ...formState,
+            [event.target.id]: event.target.value
+        })
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState)
+    };
+
+
   return (
-    <div>ContactForm</div>
+
+    <div>
+    <h3>Contact Me</h3>
+    <form onSubmit ={handleSubmit}>
+        <div className="form-group">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Your Name"/>
+        </div>
+        <div className="form-group">
+            <label for="email">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Your Email"/>
+        </div>
+
+        <div className="form-group">
+            <label for="message">Message</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='Your Message...'></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    </div>
   )
 }
 
