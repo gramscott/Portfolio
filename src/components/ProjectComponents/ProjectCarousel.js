e92efc56545126d1dc1479f57c8dfaae14acd366
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-09 09:36:17 +0000

    second project displaying but it's not a carousel as of yet

--- src/components/ProjectComponents/ProjectCarousel.js
+++ src/components/ProjectComponents/ProjectCarousel.js
@@ -4,6 +4,7 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './ProjectCarousel.css'
 import smecoPic from '../../images/carousel-smeco-pic.png'
+import empowerMePic from '../../images/carousel-empowerme-pic.png'
 
 const ProjectCarousel = () => {
 
@@ -49,6 +50,22 @@ const ProjectCarousel = () => {
         </a>
       </ul>
     </div>
+
+    <div className='project-slide'>
+      <img src={empowerMePic} alt='pic-2' className='img-fluid pics' />
+      <b>Full-Stack Java Project: Empower Me</b>
+      <p>
+        Java, Spring Boot, IntelliJ, API, JUnit, JavaScript, Vs Code, React, MongoDB HTML, CSS. Group [5 days]
+      </p>
+      <ul>
+          <li>Collaborated to create a personalized self-help affirmation app where users could customize their positive affirmations and establish a safe space.</li>
+          <li>Created the Java backend and successfully connected the React frontend through RESTful routes.</li>
+          <li>Seeded and handled enormous data loader for presentation.</li>
+          <a href="https://github.com/gramscott/eMpowerMe">
+          <i className="fa fa-github"></i> GitHub Repository
+          </a>
+        </ul>
+      </div>
 
 
   </Slider>
