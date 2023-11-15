850cea02ba8f8f07f3aacd6ff4294881b5189fc6
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-15 14:57:00 +0000

    testing

--- client/src/components/ContactComponents/ContactForm.js
+++ client/src/components/ContactComponents/ContactForm.js
@@ -1,39 +1,36 @@
-import React from 'react'
-
-import { Button } from 'react-bootstrap';
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import './ContactForm.css';
 
 const ContactForm = () => {
 
-
   return (
-
     <div>
-    <h3>Get In Touch!</h3>
-    <form action='https://api.web3forms.com/submit' method="POST">
-    <input type='hidden' name='access_key' value="099dd778-2dab-49cf-b4cc-9784c27b5566" />
-        <input
-        type="text"
-        name='name'
-         placeholder="Name" 
-         required/>
+      <h3 className='contact-header'>Get In Touch!</h3>
+      <Form action='https://api.web3forms.com/submit' method="POST">
+      <input type='hidden' name='access_key' value="099dd778-2dab-49cf-b4cc-9784c27b5566" />
 
-        <input 
-        name='email'
-        type="email"
-         placeholder="email" 
-         required
-         />
+        <Form.Group controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" name="name" placeholder="Enter your name" required />
+        </Form.Group>
 
-        <textarea 
-        name="message" 
-        type="message"
-        placeholder="message"
-        required
-         />
-        <Button type="submit">Submit</Button>
-    </form>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" name="email" placeholder="Enter your email address" required />
+        </Form.Group>
+
+        <Form.Group controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" name="message" placeholder="Enter your message" rows={5} required />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
