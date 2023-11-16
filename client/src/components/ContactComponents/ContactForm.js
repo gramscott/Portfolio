a45630b5a694af241a6ded61b94f4121ab8fa58b
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-16 10:44:09 +0000

    changed the navbar and in the process of styling the contact form

--- client/src/components/ContactComponents/ContactForm.js
+++ client/src/components/ContactComponents/ContactForm.js
@@ -6,22 +6,22 @@ const ContactForm = () => {
 
   return (
     <div>
-      <h3 className='contact-header'>Get In Touch!</h3>
-      <Form action='https://api.web3forms.com/submit' method="POST">
+      <h3 className='contact-header'>Contact Me!</h3>
+      <Form action='https://api.web3forms.com/submit' method="POST" className='form col-3'>
       <input type='hidden' name='access_key' value="099dd778-2dab-49cf-b4cc-9784c27b5566" />
 
-        <Form.Group controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" name="name" placeholder="Enter your name" required />
+        <Form.Group controlId="formName" >
+          <Form.Label className='label'>Name</Form.Label>
+          <Form.Control type="text" name="name" placeholder="Enter your name" className='user-input'required />
         </Form.Group>
 
         <Form.Group controlId="formEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter your email address" required />
+          <Form.Label className='label'>Email</Form.Label>
+          <Form.Control className='input' type="email" name="email" placeholder="Enter your email address" required />
         </Form.Group>
 
         <Form.Group controlId="formMessage">
-          <Form.Label>Message</Form.Label>
+          <Form.Label className='label'>Message</Form.Label>
           <Form.Control as="textarea" name="message" placeholder="Enter your message" rows={5} required />
         </Form.Group>
 
@@ -33,4 +33,4 @@ const ContactForm = () => {
   );
 }
 
-export default ContactForm;
+export default ContactForm;  
