b312f1c72d3082d85ed1a184ddb4cd73ba9edf14
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-27 14:00:22 +0000

    manipulated the rendering data for the post. But the email, message, etc.. are lost

--- client/src/components/ContactComponents/ContactForm.js
+++ client/src/components/ContactComponents/ContactForm.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import './ContactForm.css';
+import ThankYou from './ThankYou';
 
 const ContactForm = () => {
 
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          access_key: '099dd778-2dab-49cf-b4cc-9784c27b5566',
+          redirect: '',
+          subject: 'New Submission from Portfolio Website',
+        }),
+      });
+
+      if (response.ok) {
+        setSubmitted(true);
+      } else {
+
+        console.error('form submission failed.');
+      }
+    } catch (error) {
+      console.error('form submission failed.', error);
+    }
+  };
+
   return (
     <div className='container col-md-4'>
+      {submitted ? (
+        <ThankYou /> 
+      ) : (
+        <div>
       <h3 className='contact-header mb-3 mt-3 '>Contact Me!</h3>
 
-      <Form  action='https://api.web3forms.com/submit' method="POST" className='form rounded shadow p-4 mb-5'>
-        <input type='hidden' name='access_key' value="099dd778-2dab-49cf-b4cc-9784c27b5566" />
-        <input type='hidden' name='subject' value='New Submission from Portfolio Website' />
+      <Form onSubmit={handleSubmit} className='form rounded shadow p-4 mb-5'>
+        {/* <input type='hidden' name='access_key' value="099dd778-2dab-49cf-b4cc-9784c27b5566" />
+        <input type='hidden' name='redirect' value='' />
+        <input type='hidden' name='subject' value='New Submission from Portfolio Website' /> */}
 
 
         <ul className='contact-list'>
@@ -39,6 +75,8 @@ const ContactForm = () => {
           </Button>
         </ul>
       </Form>
+      </div>
+      )}
     </div>
   );
 }
