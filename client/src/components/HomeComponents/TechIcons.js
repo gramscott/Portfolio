163590c28a14c822f00ccf829127609b23a00402
gramscott <120787031+gramscott@users.noreply.github.com>
2023-11-28 14:02:42 +0000

    taken away dead code

--- client/src/components/HomeComponents/TechIcons.js
+++ client/src/components/HomeComponents/TechIcons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faReact, faJava, faPython, faGit, faGithub, faBootstrap, faHtml5, faCss3
+  faReact, faJava, faPython, faBootstrap, faHtml5, faCss3
 } from '@fortawesome/free-brands-svg-icons';
 import './TechIcons.css'; 
 
@@ -13,8 +13,6 @@ const TechIcons = () => {
     <FontAwesomeIcon icon={faReact} className='icons'/>
     <FontAwesomeIcon icon={faJava} className='icons'/>
     <FontAwesomeIcon icon={faPython} className='icons'/>
-    {/* <FontAwesomeIcon icon={faGit} className='icons'/> */}
-    {/* <FontAwesomeIcon icon ={faGithub} className='icons'/> */}
     <FontAwesomeIcon icon={faBootstrap} className='icons'/>
     <FontAwesomeIcon icon={faHtml5} className='icons'/>
     <FontAwesomeIcon icon={faCss3} className='icons'/>
