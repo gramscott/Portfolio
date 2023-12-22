2734bae98afd62cfe458911325b5dd82a1ab1f56
gramscott <120787031+gramscott@users.noreply.github.com>
2023-12-22 14:07:56 +0000

    testing

---
+++ client/src/SeoPic.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import seoPic from '../../images/seo_pic.png'
+
+const SeoPic = () => {
+
+  return (
+
+    <div>
+    <img src={seoPic} alt="SEO Pic" className="img-fluid seo-pic" />
+    </div>
+
+  )
+}
+
+export default SeoPic
